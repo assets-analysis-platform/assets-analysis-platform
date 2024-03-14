3171db968e75b010061f65f43b5f070065c899ba
mkrolczykk <m.krolczyk66@gmail.com>
2024-03-14 17:40:04 +0100

    mkrolczyk - updated ofelia config and redis_backup.sh script

--- projects/redis-store/redis-backup-pipeline/scripts/redis_backup.sh
+++ projects/redis-store/redis-backup-pipeline/scripts/redis_backup.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+###################################################################################################
+# Script Name: redis_backup.sh
+# Description: This script creates a backup of Redis data and uploads it to AWS S3.
+# Version: 1.0
+# Author: Marcin Krolczyk
+# Date: 14.03.2024
+###################################################################################################
+
+# Example usage: ./redis_backup.sh aap-dev-bucket
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $(basename -- "$0") <bucket_name>"
+    exit 1
+fi
 
 REDIS_CLI="/usr/local/bin/redis-cli"
-RDB_DUMP_DEFAULT_PATH="/data/dump.rdb"
-S3_BUCKET_NAME="aap-dev-bucket"
+RDB_BACKUP_DUMP_PATH="/data/dump.rdb"                             # the default dump path is the directory from which the Redis server got started from
+S3_BUCKET_NAME=$1
 
 DIR=`date +%Y-%m-%d_%H-%M`
 DATE_DIR="date=$DIR"
@@ -11,7 +25,7 @@ mkdir --parents $DEST
 
 # create backup
 echo save| $REDIS_CLI
-mv $RDB_DUMP_DEFAULT_PATH $DEST
+mv $RDB_BACKUP_DUMP_PATH $DEST
 
 # upload snapshot to AWS S3
 RDB_FILE=$DEST/dump.rdb                                           # dump.rdb to be uploaded
